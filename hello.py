3d7483872bf258ebe053d6edf392e4b7afb9344b
“shrutibadhwar” <shrutibadhwar@SHRUTI-BADHWARs-MacBook-Pro.local>
2016-06-06 13:33:44 -0700

    Initial commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  6 13:30:40 2016
+
+@author: shrutibadhwar
+"""
+
+def hello():
+    print "Hello World"
+    
+if __name__ == "__main__":
+    hello()
